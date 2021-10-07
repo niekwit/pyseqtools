37be19a30eabf208032d065dd4c911bdd82e9e05
niekwit <41302859+niekwit@users.noreply.github.com>
2021-10-07 08:37:27 +0100

    bowtie1, bugged

--- utils/utils_cutrun.py
+++ utils/utils_cutrun.py
@@ -54,7 +54,7 @@ def bowtie(work_dir, script_dir, threads, cutrun_settings, genome):
             #unzip bowtie
             unzip = ["unzip", "-qq", download_file, "-d", script_dir]
             subprocess.run(unzip)
-           
+                      
     #check for bowtie index
     index = cutrun_settings["bowtie"][genome]
     if index == "":
@@ -64,17 +64,18 @@ def bowtie(work_dir, script_dir, threads, cutrun_settings, genome):
         os.makedirs(index_dir, exist_ok = True)
         index = os.path.join(index_dir, genome)
         
-        bowtie_index = [os.path.join(bowtie, "bowtie-build"), "--threads", threads, fasta, index]
+        bowtie_index = [os.path.join(bowtie, "bowtie-build"), "--threads", 
+                        threads, fasta, index]
         
         #build index
-        utils.write2log()
-        subprocess.call(work_dir, bowtie_index, "Bowtie index build for " + genome + ": " )
+        utils.write2log(work_dir, bowtie_index, "Bowtie index build for " + genome + ": " )
+        subprocess.call(bowtie_index)
         
         #write index location to cut-run.yaml
         with open(os.path.join(script_dir, "yaml", "cut-run.yaml")) as f:
             doc = yaml.safe_load(f)
         
-        doc["hisat2"][genome] = index_dir
+        doc["bowtie"][genome] = index
         with open(os.path.join(script_dir,"yaml" ,"cut-run.yaml"), "w") as f:
             yaml.dump(doc,f)
             
@@ -82,17 +83,49 @@ def bowtie(work_dir, script_dir, threads, cutrun_settings, genome):
         with open(os.path.join(script_dir, "yaml", "cut-run.yaml")) as file:
             cutrun_settings = yaml.full_load(file)
         
-    def align_se():
-        pass
-    
-    def align_pe():
-        pass
-            
     #check if data is paired-end
     paired_end = utils.getEND(work_dir)
+    
+    #get bowtie index
+    bowtie_index = cutrun_settings["bowtie"][genome]
     
     #load other requirements for alignment
     if paired_end == "SE":
         file_list = glob.glob(os.path.join(work_dir, "trim","*_trimmed.fq.gz"))
+        
     elif paired_end == "PE":
+        print("Aligning reads with Bowtie (paired-end mode):")
         read1_list = glob.glob(os.path.join(work_dir, "trim","*_R1_001_val_1.fq.gz"))
+        read2_list = [x.replace("_R1_001", "_R2_001") for x in read1_list]
+        output_list = [x.replace("trim", "bam").replace("_R1_001_val_1.fq.gz",".bam") for x in read1_list]
+        
+        out_dir = os.path.join(work_dir, "bam")
+        os.makedirs(out_dir, exist_ok = True)
+        
+        #load blacklist for selected genome
+        blacklist = utils.blackList(script_dir, genome)
+                      
+        for read1, read2, out_file in zip(read1_list, read2_list, output_list):
+            
+            if not utils.file_exists(out_file):
+                print("Aligning " + os.path.basename(read1).replace("_R1_001_val_1.fq.gz", ""))
+                samtools = ["|", "samtools", "view", "-q", "15", "-F", "260", "-bS", 
+                            "-@", threads, "-", "|", "bedtools", "intersect", "-v", "-a",
+                            "stdin", "-b", blacklist, "-nonamecheck", "|", "samtools",
+                            "sort", "-@", threads, "-", ">"]
+                
+                bowtie_command = [os.path.join(bowtie, "bowtie"), "-m", "1", "-v", "2", "-S", 
+                          "-I", "0", "-X", "2000", "-p", threads, "--sam-nohead","-x", bowtie_index, 
+                          "-1", read1, "-2", read2, "-"]
+                
+                bowtie_command.extend(samtools)
+                
+                bowtie_command.append(out_file)
+                print(" ".join(bowtie_command))
+                subprocess.call(bowtie_command)
+                
+        
+        
+
+def bowtie2(work_dir, script_dir, threads, cutrun_settings, genome):
+    pass
