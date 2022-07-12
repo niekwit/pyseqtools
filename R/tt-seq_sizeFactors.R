fdddbb92723524f23b537e735ab10d76ab13b4bb
niekwit <41302859+niekwit@users.noreply.github.com>
2022-07-12 16:10:15 +0100

    first commit

--- R/tt-seq_sizeFactors.R
+++ R/tt-seq_sizeFactors.R
@@ -1,8 +1,29 @@
 library(DESeq2)
 
-#get parsed arguments
-args <- commandArgs(trailingOnly=TRUE)
-work.dir <- args[1]
-script.dir <- args[2]
-gtf <- args[3]
-genome <- args[4]
+work.dir <- getwd()
+
+#import sample table
+sampleTable <- read.csv(file.path(work.dir,"deseq2.csv"))
+sampleTable$file <- file.path(work.dir,"htseq-count", sampleTable$file)
+sampleTable$condition <- factor(sampleTable$condition)
+sampleTable$genotype <- factor(sampleTable$genotype)
+
+#create DESeq2 object
+directory <- file.path(work.dir,"htseq-count")
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+                                       directory = directory,
+                                       design= ~ condition)
+
+#generate size factors
+sizeFactors <- estimateSizeFactors(ddsHTSeq)
+sizeFactors <- sizeFactors(sizeFactors)
+
+#prepare df to write to file
+df <- as.data.frame(sizeFactors, row.names = NULL)
+df$sample <- row.names(df)
+df <- df[ , c(2,1)]
+
+#write df to file
+write.csv(df, file = file.path(work.dir,"sizeFactors.csv,"), row.names = FALSE)
+
+
