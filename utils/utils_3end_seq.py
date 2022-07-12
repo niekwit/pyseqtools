82e052e41b641065a6f5a3c9d7b97830f8a7ae7b
niekwit <41302859+niekwit@users.noreply.github.com>
2022-07-12 16:11:15 +0100

    first commit

---
+++ utils/utils_3end_seq.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import sys
+import subprocess
+import shutil
+
+
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
+script_dir = os.path.dirname(script_dir)
+sys.path.append(os.path.join(script_dir, "utils"))
+import utils_general as utils
