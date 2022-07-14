38ecf2ee8357407f9a07f3ca9627b7751c2df85b
niekwit <41302859+niekwit@users.noreply.github.com>
2022-07-14 17:48:57 +0100

    updated

--- R/tt-seq_sizeFactors.R
+++ R/tt-seq_sizeFactors.R
@@ -2,28 +2,36 @@ library(DESeq2)
 
 work.dir <- getwd()
 
-#import sample table
-sampleTable <- read.csv(file.path(work.dir,"deseq2.csv"))
-sampleTable$file <- file.path(work.dir,"htseq-count", sampleTable$file)
-sampleTable$condition <- factor(sampleTable$condition)
-sampleTable$genotype <- factor(sampleTable$genotype)
+out.file <- file.path(work.dir,"sizeFactors.csv")
 
-#create DESeq2 object
-directory <- file.path(work.dir,"htseq-count")
-ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
-                                       directory = directory,
-                                       design= ~ condition)
+if (file.exists(out.file) == FALSE){
+  #import sample table
+  sampleTable <- read.csv(file.path(work.dir,"samples.csv"))
+  sampleTable$file <- file.path(work.dir,"htseq-count", sampleTable$file)
+  sampleTable$condition <- factor(sampleTable$condition)
+  sampleTable$genotype <- factor(sampleTable$genotype)
+  
+  #create DESeq2 object
+  directory <- file.path(work.dir,"htseq-count")
+  ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+                                         directory = directory,
+                                         design= ~ condition)
+  
+  #generate size factors
+  sizeFactors <- estimateSizeFactors(ddsHTSeq)
+  sizeFactors <- sizeFactors(sizeFactors)
+  
+  #prepare df to write to file
+  df <- as.data.frame(sizeFactors, row.names = NULL)
+  df$sample <- row.names(df)
+  df <- df[ , c(2,1)]
+  
+  #write df to file
+  write.csv(df, file = out.file, row.names = FALSE)
+} else {
+  print("sizeFactors.csv already exists")
+}
 
-#generate size factors
-sizeFactors <- estimateSizeFactors(ddsHTSeq)
-sizeFactors <- sizeFactors(sizeFactors)
 
-#prepare df to write to file
-df <- as.data.frame(sizeFactors, row.names = NULL)
-df$sample <- row.names(df)
-df <- df[ , c(2,1)]
-
-#write df to file
-write.csv(df, file = file.path(work.dir,"sizeFactors.csv,"), row.names = FALSE)
 
 
