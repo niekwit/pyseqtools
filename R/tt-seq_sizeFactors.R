90a72cfaf9a690d22b2c2cbcb4416f9b1bd1e595
niekwit <41302859+niekwit@users.noreply.github.com>
2022-07-29 10:55:36 +0100

    working

--- R/tt-seq_sizeFactors.R
+++ R/tt-seq_sizeFactors.R
@@ -1,37 +1,79 @@
 library(DESeq2)
+library(dplyr)
 
 work.dir <- getwd()
 
-out.file <- file.path(work.dir,"sizeFactors.csv")
+out.file <- file.path(work.dir,"scaleFactors.csv")
 
 if (file.exists(out.file) == FALSE){
   #import sample table
   sampleTable <- read.csv(file.path(work.dir,"samples.csv"))
-  sampleTable$file <- file.path(work.dir,"htseq-count", sampleTable$file)
   sampleTable$condition <- factor(sampleTable$condition)
   sampleTable$genotype <- factor(sampleTable$genotype)
   
-  #create DESeq2 object
-  directory <- file.path(work.dir,"htseq-count")
-  ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
-                                         directory = directory,
-                                         design= ~ condition)
+  #generate list of gene count file
+  count.files <- Sys.glob(file.path(work.dir,"bam","R64-1-1","*","*ReadsPerGene.out.tab"))
   
-  #generate size factors
-  sizeFactors <- estimateSizeFactors(ddsHTSeq)
-  sizeFactors <- sizeFactors(sizeFactors)
+  #get gene/sample names for index names
+  genes <- read.csv(count.files[1], skip=4, sep="\t", header=FALSE)
+  genes <- genes$V1
   
-  #prepare df to write to file
-  df <- as.data.frame(sizeFactors, row.names = NULL)
-  df$sample <- row.names(df)
-  df <- df[ , c(2,1)]
+  rownames(sampleTable) <- sampleTable$sample
   
-  #write df to file
-  write.csv(df, file = out.file, row.names = FALSE)
-} else {
-  print("sizeFactors.csv already exists")
+  #generate count matrix for DESeq2
+  countMatrix <- data.frame(matrix(ncol=1, nrow = 7127))
+  col.names <- c("index")
+  names(countMatrix) <- col.names
+  countMatrix$index <- genes
+  rownames(countMatrix) <- countMatrix$index
+  
+  
+  for (i in count.files) {
+    sample <- basename(i)
+    sample <- sub("ReadsPerGene.out.tab","",sample)
+    df <- read.csv(i, skip=4, sep="\t", header=FALSE)
+    df <- subset(df, select=c(1,2))
+    colnames(df) <- c("index", sample)
+    countMatrix <- left_join(countMatrix, df, by="index")
+  }
+  #create named index
+  rownames(countMatrix) <- countMatrix$index
+  countMatrix$index <- NULL
+  
+  rownames(sampleTable) <- sampleTable$sample
+  sampleTable$sample <- NULL
+  
+  #reorder data frame columns
+  samples <- rownames(sampleTable)
+  countMatrix <- countMatrix %>% select(samples)
+  
+  #check whether the order of the sample names between countMatrix and sampleTable is the same
+  if(all(rownames(sampleTable) == colnames(countMatrix))){
+    #create DESeq2 object
+    dds <- DESeqDataSetFromMatrix(countData = countMatrix,
+                                  colData = sampleTable,
+                                  design = ~ condition)
+    
+    #calculate size factors
+    sizeFactors <- estimateSizeFactors(dds)
+    sizeFactors <- sizeFactors(sizeFactors)
+    
+    #calculate scale factors
+    df <- as.data.frame(sizeFactors, row.names = NULL)
+    df$sample <- row.names(df)
+    df <- df[ , c(2,1)]
+    df$scaleFactors <- 1 / df$sizeFactors
+    df$sizeFactors <- NULL
+    
+    #write df to file
+    write.csv(df, file = out.file, row.names = FALSE)
+    
+  } else{
+    print("ERROR: row names of sampleTable and column names of countMatrix do not have the same order")
+    exit()
+  }
+    
 }
+  
 
-
-
-
+  
