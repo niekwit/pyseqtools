feac72b610187b0ed44be73702b09f80b39ccabd
niekwit <41302859+niekwit@users.noreply.github.com>
2022-09-12 14:07:59 +0100

    fixed error

--- bash/readRatio.sh
+++ bash/readRatio.sh
@@ -6,12 +6,12 @@ count_TES=$3
 
 #function to count reads at TSS/TES
 countREADS () {
-cat $BED_ratio | grep _TSS_ | awk '{print $4}' | awk -F '_' '{print $1}' | sort | uniq -c | sed 's/^ *//' > $1
+cat $BED_ratio | grep $1 | awk '{print $4}' | awk -F '_' '{print $1}' | sort | uniq -c | sed 's/^ *//' > $2
 }
 
 #create file for TSS
-countREADS $count_TSS
+countREADS '_TSS_' $count_TSS
 
 #create file for TES
-countREADS $count_TES
+countREADS '_TES_' $count_TES
 
