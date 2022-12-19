1da9420e05843f8712dad75f123884858f1020a2
niekwit <41302859+niekwit@users.noreply.github.com>
2022-12-19 12:00:31 +0000

    working

--- utils/merge-annotation.py
+++ utils/merge-annotation.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Created on Fri Dec 16 13:10:59 2022
-
-@author: niek
+Merges MACS3 xls file with HOMER peak annotations
 """
 
 import pandas as pd
@@ -15,16 +13,18 @@ args = sys.argv
 macs3 = args[1]
 homer = args[2]
 
-#merge HOMER peak annotation with MACS3 output
-out_file = homer.replace("_annotated-peaks.txt","_annotated_macs3_peaks.csv")
-    
-df_macs3 = pd.read_csv(macs3, skiprows=(29), sep = "\t")
+#load data
+df_macs3 = pd.read_csv(macs3, skiprows=29,sep = "\t")
+
 df_homer = pd.read_csv(homer, sep = "\t")
 df_homer = df_homer.rename(columns = {df_homer.columns[0]: "name"})
-df_merge = pd.merge(df_macs3, df_homer[["name","Gene Name", "Gene Alias", 
-                                        "Gene Description", "Gene Type"]], 
-                    on = "name", 
-                    how = "left" )
+df_homer = df_homer.drop(["Chr","Start","End","Strand","Peak Score","Focus Ratio/Region Size"],axis=1)
+
+#merge HOMER peak annotation with MACS3 output
+out_file = homer.replace("_annotated-peaks.txt","_annotated_macs3_peaks.csv")
+df_merge = pd.merge(df_macs3, df_homer, on = "name", how = "left" )
+
+#save to csv file
 df_merge.to_csv(out_file, index = False)
 
 
